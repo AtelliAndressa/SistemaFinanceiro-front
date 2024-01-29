6557287b7d33acf32c4f0434baee5a59d2f47b99
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-29 10:48:59 -0300

    implementação menu
    método para o menu e scss sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sidebar', //nome do componente a ser usado
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
+
 export class SidebarComponent {
+
+  constructor(private router : Router){}
+
+  //Método do menu que direciona para a rota correta.
+  selectMenu(menu:number)
+  {
+    switch(menu)
+    {
+      case 1:
+        this.router.navigate(['/dashboard']);
+        break;
+
+      case 2:
+        this.router.navigate(['/sistema']);
+        break;
+
+      case 3:
+        this.router.navigate(['/categoria']);
+        break;
+
+      case 4:
+        this.router.navigate(['/despesa'])
+        break;
+
+      default:
+        break;
+    }
+
+  }
 
 }
