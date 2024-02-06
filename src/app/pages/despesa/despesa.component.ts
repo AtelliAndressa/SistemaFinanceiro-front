90a97eb544c608cbbd305d8fb3d3c1ce008bc4ed
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-02-06 08:20:14 -0300

    criação despesas

--- src/app/pages/despesa/despesa.component.ts
+++ src/app/pages/despesa/despesa.component.ts
@@ -3,19 +3,44 @@ import { MenuService } from '../../services/menu.service';
 
 import { NavbarModule } from '../../components/navbar/navbar.module';
 import { SidebarModule } from '../../components/sidebar/sidebar.module';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { SelectModel } from '../../models/SelectModel';
 
 @Component({
   selector: 'despesa',
   standalone: true,
-  imports: [NavbarModule, SidebarModule],
+  imports: [NavbarModule, SidebarModule, ReactiveFormsModule, FormsModule, CommonModule, NgSelectModule],
   templateUrl: './despesa.component.html',
   styleUrl: './despesa.component.scss'
 })
 export class DespesaComponent {
 
-  constructor(public menuService:MenuService){}
+  constructor(public menuService:MenuService, public formBuilder: FormBuilder){}
 
+  listSistemas = new Array<SelectModel>();
+  sistemaSelect = new SelectModel();
+  
+  despesaForm: FormGroup;
   ngOnInit() {
     this.menuService.menuSelecionado = 4;
+
+    this.despesaForm = this.formBuilder.group(
+      {
+        name:['', [Validators.required]]
+      }
+    )
+  }
+
+  dadosForm() {
+    return this.despesaForm.controls;
+  }
+
+  enviar() {
+    debugger
+    var dados = this.dadosForm();
+
+    alert(dados["name"].value)
   }
 }
